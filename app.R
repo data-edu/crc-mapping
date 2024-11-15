163328e7605a9d6f75038a496d5862c02b3a04a7
Joshua Rosenberg <jmrosen48@gmail.com>
2024-11-15 09:14:19 -0500

    trying to add counties

--- app.R
+++ app.R
@@ -3,6 +3,9 @@ library(mapboxer)
 library(dplyr)
 library(shinythemes)
 library(haven)
+library(urbnmapr)
+
+counties_sf <- get_urbn_map("counties", sf = TRUE)
 
 hdallyears <- read_dta("hdallyears.dta")
 
@@ -25,19 +28,17 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                 "Select year:",
                                 choices = c(2010, 2015, 2020))
                   ),
-                
-                # Show a plot of the generated distribution
-                mainPanel(
-                  mapboxerOutput("map")
+                  
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                    mapboxerOutput("map")
+                  )
                 )
-)
 )
 
 server <- function(input, output) {
   
   output$map <- renderMapboxer({
-    
-    print(input$selected_size)
     
     hdallyears %>%
       filter(year == input$selected_year) %>%
@@ -46,6 +47,7 @@ server <- function(input, output) {
       
       # Setup a map with the default source above
       mapboxer(
+        # style = "mapbox://styles/mapbox/light-v10",
         center = c(-95, 40),
         zoom = 2.5
       ) %>%
@@ -58,6 +60,13 @@ server <- function(input, output) {
         circle_color = "white",
         circle_radius = 3,
         popup = "Institution: {{instnm}}"
+      ) %>% 
+      
+      add_fill_layer(
+        source = counties_sf,
+        fill_color = "white",  # Set the fill color
+        fill_opacity = 0.5,  # Adjust transparency
+        fill_outline_color = "white"  # Optional: add an outline
       )
     
   })
